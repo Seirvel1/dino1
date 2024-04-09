642941844003c541f63c0279f6cb9b7476bafdc6
Seirvel1 <145506225+Seirvel1@users.noreply.github.com>
2024-04-09 04:54:02 +0300

    Add files via upload

---
+++ img/Images.jsx
@@ -0,0 +1,13 @@
+const IMAGES = {
+    image1 : new URL('./images/1018.png', import.meta.url).href
+   
+
+}
+
+export default IMAGES
+
+// image2 : new URL('./images/1018.png', import.meta.url).href
+// image3 : new URL('./images/1018.png', import.meta.url).href
+// image4 : new URL('./images/1018.png', import.meta.url).href
+// image5 : new URL('./images/1018.png', import.meta.url).href
+// image6 : new URL('./images/1018.png', import.meta.url).href
